c02979ffc684b5c0ecddaf05c37cf946c6eab59b
sinanyasar0555 <sinanyasar055@gmail.com>
2025-06-04 15:33:34 +0300

    Add files via upload

---
+++ app/data/places.ts
@@ -0,0 +1,32 @@
+export const CevredekiYerler = [
+  {
+    id: "bandirma-gemi-muze",
+    title: "Bandırma Gemi-Müze ve Millî Mücadele Açık Hava Müzesi",
+    image: "SAMSUN-BANDIRMA VAPURU-GÜLCAN ACAR (2).jpg",
+    description: "Türkiye Cumhuriyeti'nin kuruluşunda önemli bir rolü olan Bandırma Gemisi, 1878 yılında Glasgov'da \"Trocadero\" ismiyle inşa edilmiş ve sorasında Kymi adıyla uzun yıllar yük taşımıştır. 12 Aralık 1891 yılında batan gemi çıkarılarak onarılmış ve aynı yıl içinde yeniden yüzdürülmüştür."
+  },
+  {
+    id: "amisos-tepesi",
+    title: "Amisos Tepesi",
+    image: "c36a965f-61b8-4768-939d-74b398a25db0.jpg",
+    description: "Amisos'un kuruluşu ve gelişmesi tarihi kaynaklara ve arkeolojik verilere göre başlıca dört aşamada gerçekleşmiştir. 1.Dönem (MÖ 6'ncı yüzyılın başı): Sinop'ta bir koloni oluşturan Miletoslular'ın küçük bir yerleşim olan Amisos'a egemen olup Karadeniz ile Anadolu'nun içlerini bağlayan ticaret yolunun başlangıcı olarak kullanılması. 2.Dönem (MÖ 6'ncı yüzyılın ilk yarısı): Kapadokyalılar'ın Amisos'a yerleşmesi. 3.Dönem (MÖ 6'ncı yüzyıl ortası): Kapadokyalı liderin Foçalılar'a yerleşme izni vermesi ve kentin büyüyüp gelişmesi. 4. Dönem (MÖ 437): Sinop'tan gelen Atinalılar'ın Amisos'a yerleşmesi ve kentin adının Peiraieos olarak değiştirilmesi."
+  },
+  {
+    id: "gazi-muzesi",
+    title: "Gazi Müzesi",
+    image: "SAMSUN-GAZİ MÜZESİ-GÜLCAN ACAR (1) kopya.jpg",
+    description: "Mıntıka Palas Oteli, 1902 yılında Jean İonnis Mantika tarafından bir otel olarak inşa edilmiştir. İki katlı olan yapının dış duvarları yığma tuğla, iç bölmeleri bağdadî olarak yapılmıştır. Mustafa Kemal Paşa Samsun'a çıktığında bu otelde kalmıştır."
+  },
+  {
+    id: "samsun-kent-muzesi",
+    title: "Samsun Kent Müzesi",
+    image: "20190531140715007_kent muzesi.jpeg",
+    description: "Samsun Kent Müzesi binaları kentin demiryolu tarihi açısından ayrıcalıklı bir yere sahiptir. Cumhuriyetten önce yabancı şirketler tarafından işletilen hatlar Cumhuriyet ile birlikte hızla devletleştirilmiş, yurdun önemli bir bölümü demir ağlarla örülmüştür. Demiryollarına gösterilen bu önem kurumun çalışanlarına da yansımıştır; bu büyük ve önemli kurumun personeline lojmanlar tahsis edilmiştir."
+  },
+  {
+    id: "amazon-koyu",
+    title: "Amazon Köyü",
+    image: "7464ed6d-1c65-41ad-834b-49d18c14dc09.jpeg",
+    description: "MÖ 3 bin ila 2 bin yılları arasında, kentler-devletler kurup anaerkil bir şekilde hayatlarını sürdüren Amazonlar bir dönem Terme'de de yaşamışlardır. Onların anısına İlkadım Batıpark içerisinde bir Amazon Köyü oluşturulmuş ve bu yürekli, mert, savaşçı kadınların günlük yaşamını yansıtan balmumu heykelleri yerleştirilerek ziyaret açılmıştır."
+  }
+]; 
